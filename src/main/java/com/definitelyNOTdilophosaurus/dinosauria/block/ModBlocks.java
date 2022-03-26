38506cc48b8d850a7e7cad17a88732fadcb3a662
legocraftboy <Legocraftboy@gmail.com>
2022-03-25 17:12:45 -0700

    Added the Cycad Log and Stripped log

--- src/main/java/com/definitelyNOTdilophosaurus/dinosauria/block/ModBlocks.java
+++ src/main/java/com/definitelyNOTdilophosaurus/dinosauria/block/ModBlocks.java
@@ -1,12 +1,18 @@
 package com.definitelyNOTdilophosaurus.dinosauria.block;
 
 import com.definitelyNOTdilophosaurus.dinosauria.DinosauriaMod;
+import com.definitelyNOTdilophosaurus.dinosauria.block.custom.ModFlammableRotatedPillarBlock;
 import com.definitelyNOTdilophosaurus.dinosauria.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,8 +26,38 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, DinosauriaMod.MOD_ID);
 
     public static final RegistryObject<Block> CYCAD_PLANKS = registerBlock("cycad_planks",
-            () -> new Block(BlockBehaviour.Properties.of(Material.WOOD)
-                    .strength(3f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)){
+                @Override
+                public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+                    return true;
+                }
+
+                @Override
+                public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+                    return 20;
+                }
+
+                @Override
+                public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+                    return 5;
+                }
+            }, CreativeModeTab.TAB_MISC);
+
+    public static final RegistryObject<Block> CYCAD_LOG = registerBlock("cycad_log",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)
+                    .strength(3f)), CreativeModeTab.TAB_MISC);
+
+    public static final RegistryObject<Block> CYCAD_WOOD = registerBlock("cycad_wood",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)
+                    .strength(3f)), CreativeModeTab.TAB_MISC);
+
+    public static final RegistryObject<Block> STRIPPED_CYCAD_LOG = registerBlock("stripped_cycad_log",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG)
+                    .strength(3f)), CreativeModeTab.TAB_MISC);
+
+    public static final RegistryObject<Block> STRIPPED_CYCAD_WOOD = registerBlock("stripped_cycad_wood",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD)
+                    .strength(3f)), CreativeModeTab.TAB_MISC);
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
